7d0182ea34c5baaf6f7fe4b0e3c6061c4b3472bb
jeffc <jeffc@JCDESKTOP>
2016-12-07 23:08:08 +1100

    Add more code.

---
+++ CodeMetric.Extension/CodeMetric.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Microsoft.VisualStudio.Text.Editor;
+
+namespace CodeMetric.Extension
+{
+    class CodeMetric
+    {
+        private CodeMetricBarControl _root;
+        private IWpfTextView _view;
+        private IAdornmentLayer _adornmentLayer;
+        
+        public CodeMetric(IWpfTextView view)
+        {
+            _view = view;
+            _root = new CodeMetricBarControl();
+
+            _adornmentLayer = view.GetAdornmentLayer("CodeMetric");
+
+            _view.ViewportHeightChanged += OnViewSizeChanged;
+            _view.ViewportWidthChanged += OnViewSizeChanged;
+        }
+
+        private void OnViewSizeChanged(object sender, EventArgs eventArgs)
+        {
+            _adornmentLayer.RemoveAdornment(_root);
+
+            Canvas.SetLeft(_root, _view.ViewportRight - 80);
+            Canvas.SetTop(_root, _view.ViewportTop + 15);
+
+            _adornmentLayer.AddAdornment(AdornmentPositioningBehavior.ViewportRelative, null, null, _root, null);
+        }
+    }
+}
---
+++ CodeMetric.Extension/CodeMetricBarFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace CodeMetric.Extension
+{
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [ContentType("text")]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    internal sealed class CodeMetricBarFactory : IWpfTextViewCreationListener
+    {
+        [Export(typeof(AdornmentLayerDefinition))]
+        [Name("CodeMetric")]
+        [Order(After = PredefinedAdornmentLayers.Caret)]
+        [TextViewRole(PredefinedTextViewRoles.Document)]
+        public AdornmentLayerDefinition editorAdornmentLayer = null;
+
+        public void TextViewCreated(IWpfTextView textView)
+        {
+            textView.Properties.GetOrCreateSingletonProperty(() => new CodeMetric(textView));
+        }
+    }
+}
